40b13a22654fed15988ba2c05dae092dc4f95699
DrHazers <155387826+DrHazers@users.noreply.github.com>
2024-06-06 03:19:44 +0800

    完成修改密码功能

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
     proxy: {
       // path: 目标服务器
       '/api': {
-        target: 'https://heimahr.itheima.net/'
+        target: 'https://heimahr.itheima.net'
       }
     }
     // before: require('./mock/mock-server.js') 基础模板做的模拟数据 拦截请求
