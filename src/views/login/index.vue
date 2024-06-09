a4d4e4a845d784ae905e3342f3abb39d5b9a4f18
DrHazers <155387826+DrHazers@users.noreply.github.com>
2024-06-10 07:01:01 +0800

    登录界面样式修改以及修改项目跳转的地址

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,62 +1,65 @@
 <template>
-  <div class="login-container">
-    <div class="logo" />
-    <div class="form">
-      <h1>登录</h1>
-      <el-card shadow="never" class="login-card">
-        <!--登录表单-->
-        <!-- el-form > el-form-item > el-input -->
-        <el-form ref="form" :model="loginForm" :rules="loginRules">
-          <el-form-item prop="mobile">
-            <el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
-          </el-form-item>
-          <el-form-item prop="password">
-            <el-input v-model="loginForm.password" show-password placeholder="请输入密码" />
-          </el-form-item>
-          <el-form-item prop="isAgree">
-            <el-checkbox v-model="loginForm.isAgree">
-              用户平台使用协议
-            </el-checkbox>
-          </el-form-item>
-          <el-form-item>
-            <el-button style="width:350px" type="primary" @click="login">登录</el-button>
-          </el-form-item>
-        </el-form>
-      </el-card>
+  <div>
+    <div ref="vantaRef" style="width:100%;height:100vh">
+      <div class="login-container">
+        <div class="form">
+          <h1>LOGIN</h1>
+          <el-card shadow="never" class="login-card">
+            <!--登录表单-->
+            <el-form ref="form" :model="loginForm" :rules="loginRules">
+              <el-form-item prop="mobile">
+                <el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
+              </el-form-item>
+              <el-form-item prop="password">
+                <el-input v-model="loginForm.password" show-password placeholder="请输入密码" />
+              </el-form-item>
+              <el-form-item prop="isAgree">
+                <el-checkbox v-model="loginForm.isAgree" style="color:black">
+                  User Platform Service Agreement
+                </el-checkbox>
+              </el-form-item>
+              <el-form-item>
+                <el-button class="login-button" type="primary" @click="login">Sign in</el-button>
+              </el-form-item>
+            </el-form>
+          </el-card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
 <script>
+import * as THREE from 'three'
+import Net from 'vanta/src/vanta.net'
 export default {
   name: 'Login',
   data() {
     return {
       loginForm: {
         mobile: process.env.NODE_ENV === 'development' ? '13800000002' : '',
-        password: process.env.NODE_ENV === 'development' ? '123456' : '',
+        password: process.env.NODE_ENV === 'development' ? 'hm#qd@23!' : '',
         isAgree: process.env.NODE_ENV === 'development'
       },
       loginRules: {
         mobile: [{
           required: true,
-          message: '请输入手机号',
+          message: 'Please enter your mobile phone number',
           trigger: 'blur'
         }, {
           pattern: /^1[3-9]\d{9}$/,
-          message: '手机号格式不正确',
+          message: 'The format of the mobile phone number is incorrect',
           trigger: 'blur'
-
         }],
         password: [{
           required: true,
-          message: '请输入密码',
+          message: 'Please enter your password',
           trigger: 'blur'
         }, {
           min: 6,
           max: 16,
-          message: '密码长度应该为6-16位之间',
+          message: 'The password should be between 6-16 digits long',
           trigger: 'blur'
-
         }],
         // required只能检测 null undefined ""
         isAgree: [{
@@ -65,10 +68,35 @@ export default {
             // value 校验的值
             // callback 函数 - promise resolve reject
             // callback() callback(new Error(错误信息))
-            value ? callback() : callback(new Error('您必须勾选用户的使用协议'))
+            value ? callback() : callback(new Error('You must agree to the User Platform Usage Agreement'))
           }
         }]
       }
+    }
+  },
+  mounted() {
+    this.vantaEffect = Net({
+      el: this.$refs.vantaRef,
+      THREE: THREE
+    })
+
+    // eslint-disable-next-line no-undef
+    VANTA.NET({
+      el: this.$refs.vantaRef,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x3fffef,
+      backgroundColor: 0x63990
+    })
+  },
+  beforeDestroy() {
+    if (this.vantaEffect) {
+      this.vantaEffect.destroy()
     }
   },
   methods: {
@@ -82,64 +110,70 @@ export default {
         }
       })
     }
-
   }
 }
 </script>
+
 <style lang="scss">
+.my_title{
+  z-index: 999;
+  position: fixed;
+  top: 40%;
+  left: 10%;
+  color: aquamarine;
+  font-size: 100px;
+  font-weight: bolder;
+}
 .login-container {
   display: flex;
-  align-items: stretch;
+  justify-content: center; // 水平居中
+  align-items: center; // 垂直居中
   height: 100vh;
-  .logo {
-    flex: 3;
-    background: rgba(38, 72, 176) url(../../assets/common/login_back.png)
-      no-repeat center / cover;
-    border-top-right-radius: 60px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    justify-content: center;
-    padding: 0 100px;
-    .icon {
-      background: url(../../assets/common/logo.png) no-repeat 70px center /
-        contain;
-      width: 300px;
-      height: 50px;
-      margin-bottom: 50px;
-    }
-    p {
-      color: #fff;
-      font-size: 18px;
-      margin-top: 20px;
-      width: 300px;
-      text-align: center;
-    }
+}
+.login-card {
+  margin: 0;
+  padding: 0;
+  border-color: rgba(255, 255, 255, 0);
+  background-color: rgba(255, 255, 255, 0);
+}
+.form {
+  width: 400px;
+  padding: 20px;
+  margin: 0;
+  background: rgba(255, 255, 255, 0); // 半透明背景
+}
+h1 {
+  text-align: center;
+  font-size: 35px;
+  margin-bottom: 20px;
+  color: white;
+}
+.el-input {
+  width: 100%;
+  height: 44px;
+  border-color: rgba(255, 255, 255, 0);
+  border-radius: 8px;
+  .el-input__inner {
+    background: rgba(0, 0, 0, 0.25);
+    border-color: rgba(255, 255, 255, 0);
+    color: white;
   }
-  .form {
-    flex: 2;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding-left: 176px;
-    .el-card {
-      border: none;
-      padding: 0;
-    }
-    h1 {
-      padding-left: 20px;
-      font-size: 24px;
-    }
-    .el-input {
-      width: 350px;
-      height: 44px;
-      .el-input__inner {
-        background: #f4f5fb;
-      }
-    }
-    .el-checkbox {
-      color:#606266;
-    }
-  }
+}
+.el-checkbox {
+  color: rgba(0, 0, 0, 0.144);
+}
+.el-button {
+  width: 100%;
+}
+.login-button {
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.25); // 设置按钮的背景颜色
+  border: none;
+  color: rgb(255, 255, 255);
+  font-size: 16px;
+  padding: 12px 0;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
 }
 </style>
