70ecca5a903362b2c3a86f92ebc280762a889fdf
ddndda <2272117157@qq.com>
2024-06-12 23:05:11 +0800

    权限应用和首页

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,10 +1,12 @@
 const getters = {
-  sidebar: state => state.app.sidebar, // 取app模块属性
+  sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-  token: state => state.user.token, // 取user模块属性
+  token: state => state.user.token,
   userId: state => state.user.userInfo.userId,
   avatar: state => state.user.userInfo.staffPhoto, // 头像
-  name: state => state.user.userInfo.username // 用户名称
+  name: state => state.user.userInfo.username, // 用户名称
+  routes: state => state.user.routes,
+  company: state => state.user.userInfo.company, // 公司名称
+  departmentName: state => state.user.userInfo.departmentName // 部门名称
 }
-// getters便捷访问
 export default getters
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,11 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import { login, getUserInfo } from '@/api/user'
+import { constantRoutes } from '@/router'
+import { resetRouter } from '@/router'
 const state = {
   token: getToken(), // 从缓存中读取初始值
-  userInfo: {} // 存储用户基本资料状态
+  userInfo: {}, // 存储用户基本资料状态
+  routes: constantRoutes // 静态路由的数组
 }
 
 const mutations = {
@@ -18,6 +21,9 @@ const mutations = {
   },
   setUserInfo(state, userInfo) {
     state.userInfo = userInfo
+  },
+  setRoutes(state, newRoutes) {
+    state.routes = [...constantRoutes, ...newRoutes] // 静态路由 + 动态路由
   }
 }
 
@@ -34,11 +40,14 @@ const actions = {
   async getUserInfo(context) {
     const result = await getUserInfo()
     context.commit('setUserInfo', result)
+    return result // 返回数据
   },
   // 退出登录的action
   logout(context) {
     context.commit('removeToken') // 删除token
     context.commit('setUserInfo', {}) // 设置用户信息为空对象
+    // 重置路由
+    resetRouter()
   }
 }
 
